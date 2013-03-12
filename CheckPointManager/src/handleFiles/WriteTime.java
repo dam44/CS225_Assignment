a5368c97b44c92c2dc889cc76af964fdcebad511
Dan <dam44@aber.ac.uk>
2013-03-12 22:04:54 +0000

    Update, can write time checkpoints to file

--- CheckPointManager/src/handleFiles/WriteTime.java
+++ CheckPointManager/src/handleFiles/WriteTime.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WriteTime {
 	private String arrival, departure, entrant, node;
@@ -13,16 +15,9 @@ public class WriteTime {
 	private File timesFile;
 	private FileWriter fw;
 	private BufferedWriter bw;
+	private String writeThis;
 
 	public WriteTime(String fileNames) {
-		try {
-			timesFile = new File("src/handleFiles/times.txt");
-			fw = new FileWriter(timesFile.getAbsoluteFile());
-			bw = new BufferedWriter(fw);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	public void setData(List<String> entrant, String node,String arrival, String departure,
@@ -35,7 +30,7 @@ public class WriteTime {
 		try {
 			this.entrant = entrant.get(0);
 		} catch (IndexOutOfBoundsException e) {
-			System.err.println("Error no entrant file");
+			System.err.println("Error no entrant selected");
 		}
 		checkData();
 	}
@@ -43,15 +38,15 @@ public class WriteTime {
 	public void checkData() {
 		if (departure.equals("")) {
 			if ((arrival.matches("\\d\\d:\\d\\d"))) {
-				System.out.println("Worked");
-				parseNode();
+				if (parseNode() != -1){
 				writeToFile();
+				}
 			} else
 				System.err.println("Error arrival time is not in correct format");
 		} else {
 			if ((arrival.matches("\\d\\d:\\d\\d"))
 					&& (departure.matches("\\d\\d:\\d\\d"))) {
-				System.out.println("Worked");
+				if (parseNode() != 0)
 				writeToFile();
 			} else
 				System.err.println("Error arrival or departure time is not in correct format");
@@ -59,22 +54,43 @@ public class WriteTime {
 	}
 
 	public int parseNode() {
+		try {
 		nodeNum = Integer.parseInt(node);
 		return nodeNum;
+		}
+		catch (NumberFormatException e) {
+			System.err.println("Error, node is not a number");
+			return -1;
+		}
+	}
+	
+	public int parseEntrantNum() {
+		Matcher matcher = Pattern.compile("\\d+").matcher(entrant);
+		matcher.find();
+		System.out.println(Integer.valueOf(matcher.group()));
+		return Integer.valueOf(matcher.group());
 	}
 	
 	public void formatInToString() {
 		if (timeCheckpoint == true) {
-			
+			writeThis = "T "+nodeNum+" "+parseEntrantNum()+" "+arrival;
+			System.out.println(writeThis);
 		}
 	}
 
 	public void writeToFile() {
+		formatInToString();
 		try {
+		timesFile = new File("src/handleFiles/times.txt");
+		fw = new FileWriter(timesFile.getAbsoluteFile(), true);
+		bw = new BufferedWriter(fw);
 			if (!timesFile.exists()) {
 				timesFile.createNewFile();
-				System.out.println("Hello");
 			}
+			
+			bw.write(writeThis);
+			bw.newLine();
+			bw.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
